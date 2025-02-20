a54f50e691eae48d5ba176cdf4ad19fd99fac50e
Uzonwu <chinaecherem.nwigwe@students.plymouth.ac.uk>
2025-02-20 13:16:15 +0000

    basic website built

---
+++ MyPortfolio/script.js
@@ -0,0 +1,7 @@
+﻿let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active');
+}
